07bed59890fa66fa7eb8f1aac61c75dec9fa7160
Eric Thelin <eric@ericthelin.se>
2016-07-10 20:43:06 +0200

    Check that Fraction is equal to Fraction with inverted values

--- test/source/se/ericthelin/fractions/FractionTest.java
+++ test/source/se/ericthelin/fractions/FractionTest.java
@@ -136,6 +136,11 @@ public class FractionTest {
 	}
 
 	@Test
+	public void isEqualToFractionWithInvertedValues() {
+		assertTrue(Fraction.of("-1/3").equals(Fraction.of("1/-3")));
+	}
+
+	@Test
 	public void isNotEqualToNull() {
 		assertFalse(Fraction.of("7/5").equals(null));
 	}
