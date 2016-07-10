4baecdd1c3880ac0eb2e4218acca6e75bd90986c
Eric Thelin <eric@ericthelin.se>
2016-07-10 20:58:49 +0200

    Check that Fraction skips double minus signs from string representation

--- test/source/se/ericthelin/fractions/FractionTest.java
+++ test/source/se/ericthelin/fractions/FractionTest.java
@@ -117,6 +117,11 @@ public class FractionTest {
 	}
 
 	@Test
+	public void skipsDoubleMinusSignsFromStringRepresentation() {
+		assertThat(Fraction.of("-1/-3").toString(), is("1/3"));
+	}
+
+	@Test
 	public void isEqualToSelf() {
 		// Given
 		Fraction instance = Fraction.of("7/5");
